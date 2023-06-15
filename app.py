0c42c798040fdca1c176548c2e81767b9aef892e
deletella01 <deletella01@gmail.com>
2023-06-15 22:48:34 +0000

    Corrections

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ from flask import Flask, request, render_template
 import warnings
 warnings.filterwarnings("ignore")
 
-data = pd.read_csv('~/Melody_finder1/Melody_Finder/dataset/data.csv')
-genre_data = pd.read_csv('~/Melody_finder1/Melody_Finder/dataset/data_by_genres.csv')
-year_data = pd.read_csv('~/Melody_finder1/Melody_Finder/dataset/data_by_year.csv')
+data = pd.read_csv('~/Melody_Finder/dataset/data.csv')
+genre_data = pd.read_csv('~/Melody_Finder/dataset/data_by_genres.csv')
+year_data = pd.read_csv('~/Melody_Finder/dataset/data_by_year.csv')
 
 
 def get_decade(year):
@@ -152,7 +152,7 @@ def predict():
     
     output = recommend_songs([{'name':features[0], 'year':features[1]}],data)
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='Similar Songs are: {}'.format(output))
 
 
 
